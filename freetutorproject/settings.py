c4b19c2e29fcd3d4c4c15264484aaf225efc7227
Gustavo Rincon <gustavorincon37@gmail.com>
2026-06-27 23:24:26 -0500

    Update database settings to use PostgreSQL with new credentials

--- freetutorproject/settings.py
+++ freetutorproject/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'freetutorproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'freetutordb',
+        'USER': 'freetutoruser',
+        'PASSWORD': '111111',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
